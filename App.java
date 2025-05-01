71a4db4c0cc21527e24dca42ed845144ea842e3c
marcos-ortega <mo205016@gmail.com>
2025-04-30 20:24:25 -0700

    tercer commit

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
 public class App{
-    System.out.println("Hello World");
-    System.out.println("Hello");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello");
+    }
+
 }
